989cbb9dfbb9e20595d75945bb2e9307d645409d
ErreKappa1 <riccardo.oglietti@tutanota.com>
2020-06-11 15:14:52 +0200

    Auto push, minor changes

--- kirlia.c
+++ kirlia.c
@@ -3,11 +3,12 @@
 #include <string.h>
 #include "gardevoir.h"
 
-void subMenuWrapperStatsFn(Pokemon *db, int nr, int debug){//TODO
+void subMenuWrapperStatsFn(Pokemon *db, int nr, int debug){
 
 	int stop=1;//flag to manage exit from stats menu
 
 	if(debug)
 		printf("\nBeginning of subMenuWrapperStatsFn");
-	printf("\nInsert a command:\n1)\tAverage statistics per generation");
+	printf("\nInsert a command:\n1)\tAverage statistics per generation\n0)\tReturn to main menu\n--------------------------------------------------------------\n\n\n");
+	scanf('%d'
 }
