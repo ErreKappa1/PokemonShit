0a89b0e8a76322a81f9d4335bc5fa002f944b1b1
ErreKappa1 <riccardo.oglietti@tutanota.com>
2020-07-16 15:50:29 +0200

    Auto push, minor changes

--- gardevoir.c
+++ gardevoir.c
@@ -2,6 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include "megaGardevoir.h"
+#include "gardevoir.h"
 
 
+void printList(){
 
+   struct Node *ptr=head;
+   printf("\n[ ");
+	
+   while(ptr!=NULL){//start from the beginning
+      printf("(%d,%d) ", ptr->key, ptr->data);
+      ptr=ptr->next;
+   }
+   printf(" ]");
+}
--- gardevoir.h
+++ gardevoir.h
@@ -1,6 +1,16 @@
 #ifndef _gardevoir_H_
 #define _gardevoir_H_
 
-	
+struct{
+	Stats data;
+	int key;
+	struct Node *next;
+} Node;
+
+struct node *head=NULL;
+
+struct node *current=NULL;
+
+void printList(){//display the list
 
 #endif
