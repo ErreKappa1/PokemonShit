93d13980e6dedcdfd9cec7794e9a65c09eb25884
ErreKappa1 <riccardo.oglietti@tutanota.com>
2020-07-19 14:25:31 +0200

    Auto push, minor changes

--- gardevoir.c
+++ gardevoir.c
@@ -4,23 +4,34 @@
 #include "megaGardevoir.h"
 #include "gardevoir.h"
 
-/*
-void printList(){
+#define numOpt 5
 
-   struct Node *ptr=head;
-   printf("\n[ ");
-	
-   while(ptr!=NULL){//start from the beginning
-      printf("(%d,%d) ", ptr->key, ptr->data);
-      ptr=ptr->next;
-   }
-   printf(" ]");
+
+mainMenu readCommand(){
+
+	//static int numOpt=5;
+	int j=0;//index used to cycle on the options table
+	char string[14+1];//store the command submitted by the user
+	char optTable[numOpt][14+1]={	"printAll",//contains every possible option that the user can submit
+									"selectPkmn",
+									"printPkmn",
+									"statistics",
+									"exitTheProgram",};
+	mainMenu index;//index used to cycle on the menu
+
+	printf("\nInsert a command:");
+	printf("\n1)\tPrint the list of all available Pokemon");
+	printf("\n2)\tSelect a pokemon (Only as a reference to the user)");
+	printf("\n3)\tPrint the selected pokemon info (Still as a reference");
+	printf("\n4)\tStatistics V");
+	printf("\n0)\tExit\n--------------------------------------------------------------\n\n\n");//main menu output
+	printf("\nCommand list:");
+	for(j=0; j<numOpt; j++)//output the command list
+		printf("\n%d) %s", j+1, optTable[j]);
+	printf("\nCommand: ");
+	scanf("%s", string);//scan the command
+	for(index=printAll, j=0; index<exitTheProgram && j<numOpt && strcasecmp(string, optTable[j])!=0; index++, j++);//cycle 
+	//trought the tabcle and stops when the string and the option are equals
+
+	return index;
 }
-*/
-
-mainMenu readCommand(){//TODO write dat function
-
-	char string[10+1];//store the command submitted by the user
-	char optionsTable[5][10+1];//contains every possible option that the user can submit
-
-	
--- gardevoir.h
+++ gardevoir.h
@@ -1,27 +1,13 @@
 #ifndef _gardevoir_H_
 #define _gardevoir_H_
 
-/* list function and structure
-struct{
-	Stats data;
-	int key;
-	struct Node *next;
-} Node;
-
-struct Node *head=NULL;
-
-struct Node *current=NULL;
-
-void printList();//display the list
-*/
-
 typedef enum{
 		printAll,
 		selectPkmn,
-		printPkmnm,
+		printPkmn,
 		statistics,
-		exit
-} mainMenu
+		exitTheProgram
+} mainMenu;
 
 mainMenu readCommand();//prompt to the user and read the command from stdin
 
